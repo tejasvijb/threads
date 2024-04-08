81ad736932f9c3aa1131a98f9dee1f54aaf9a24e
tejasvijb <jbtejasvi@gmail.com>
2024-04-08 23:10:39 +0530

    post thread

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -43,3 +43,20 @@ export async function updateUser({
         throw new Error(`Failed to create/update user: ${error.message}`);
     }
 }
+
+export async function fetchUser(userId: string): Promise<any> {
+    
+    try {
+        connectToDB();
+        const user = await User
+        .findOne({ id: userId })
+        // .populate({
+        //     path: 'communities',
+        //     model: Community
+        // })
+
+        return user;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
